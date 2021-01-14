09cabd17b7114ce0066d91001273d7e25a6072dd
Radu <radu@adivare.nl>
2021-01-14 12:31:02 +0100

    updated admin to support custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -22,7 +22,7 @@ class UserManager(BaseUserManager):
         user.is_superuser = True
         user.save(using=self._db)
 
-        return User
+        return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
